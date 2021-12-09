9c745da73d82affce8a93c8dc162e82dc26d534c
paddyroddy <patrickjamesroddy@gmail.com>
2021-12-09 11:14:24 +0000

    Read in data instead and use fixture

--- day1/day1.py
+++ day1/day1.py
@@ -6,28 +6,26 @@ _file_location = Path(__file__).resolve()
 _data_path = _file_location.parent
 
 
-def find_largest_measurements(filename: str) -> int:
+def find_largest_measurements(df: pd.DataFrame) -> int:
     """
     how many measurements are larger than the previous measurement
     """
-    df = pd.read_csv(_data_path / filename, header=None)
     number_increased = int((df.diff() > 0).sum())
     print(f"Q1 number increased: {number_increased}")
     return number_increased
 
 
-def fine_largest_sums(filename: str) -> int:
+def fine_largest_sums(df: pd.DataFrame) -> int:
     """
     how many sums are larger than the previous sum
     """
     window = 3
-    df = pd.read_csv(_data_path / filename, header=None)
     window_increased = int((df.rolling(window=window).sum().diff() > 0).sum())
     print(f"Q2 window increased: {window_increased}")
     return window_increased
 
 
 if __name__ == "__main__":
-    filename = "data_day1.csv"
-    find_largest_measurements(filename)
-    fine_largest_sums(filename)
+    df = pd.read_csv(_data_path / "data_day1.csv", header=None)
+    find_largest_measurements(df)
+    fine_largest_sums(df)
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,19 +1,32 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
 from day1 import find_largest_measurements, fine_largest_sums
 
+_file_location = Path(__file__).resolve()
+_data_path = _file_location.parent
 
-def test_number_depth_increases() -> None:
+
+@pytest.fixture
+def dummy_data() -> pd.DataFrame:
+    return pd.read_csv(_data_path / "dummy_day1.csv", header=None)
+
+
+def test_number_depth_increases(dummy_data) -> None:
     """
     checks that the number of times increased matches the expected
     """
     expected = 7
-    computed = find_largest_measurements("dummy_day1.csv")
+    computed = find_largest_measurements(dummy_data)
     assert computed == expected
 
 
-def test_sliding_window() -> None:
+def test_sliding_window(dummy_data) -> None:
     """
     checks that the sum of sliding window increases as expected
     """
     expected = 5
-    computed = fine_largest_sums("dummy_day1.csv")
+    computed = fine_largest_sums(dummy_data)
     assert computed == expected
