15a9fe83e3692a1b0ad8719ebd9cea4c3f996f78
paddyroddy <patrickjamesroddy@gmail.com>
2021-12-20 15:06:56 +0000

    Fix in reading data to numpy array

--- day4/day4.py
+++ day4/day4.py
@@ -1,17 +1,20 @@
 from pathlib import Path
 
-from utils_day4 import convert_numbers_to_ints
+import numpy as np
+from utils_day4 import convert_grids_to_array_of_ints, convert_numbers_to_list_of_ints
 
 _file_location = Path(__file__).resolve()
 _data_path = _file_location.parent
 
 
-def read_data(filename: str) -> tuple[list[int], list[str]]:
+def read_data(filename: str) -> tuple[list[int], np.ndarray]:
     with open(_data_path / filename) as f:
-        content = f.readlines()
+        content = f.read().splitlines()
         numbers = content[0]
         grids = content[2:]
-    return convert_numbers_to_ints(numbers), grids
+    return convert_numbers_to_list_of_ints(numbers), convert_grids_to_array_of_ints(
+        grids
+    )
 
 
 # def find_who_won_bingo_and_compute_score(df: pd.DataFrame) -> int:
--- day4/utils_day4.py
+++ day4/utils_day4.py
@@ -1,5 +1,18 @@
-def convert_numbers_to_ints(numbers: str) -> list[int]:
+import numpy as np
+
+GRID_SIZE = dict(n_rows=5, n_cols=5)
+
+
+def convert_numbers_to_list_of_ints(numbers: str) -> list[int]:
     """
     the called numbers are read in as a string originally
     """
     return list(map(int, numbers.split(",")))
+
+
+def convert_grids_to_array_of_ints(grids: list[str]) -> np.ndarray:
+    """
+    the bingo grids are read in as a string originally
+    """
+    cleaned_array = np.array([x.split() for x in filter(None, grids)], dtype=int)
+    return cleaned_array.reshape(-1, GRID_SIZE["n_rows"], GRID_SIZE["n_cols"])
