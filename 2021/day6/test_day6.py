f1bd259b73df11ba15954a8ee5ea8d6b69ad55d7
paddyroddy <patrickjamesroddy@gmail.com>
2022-01-27 17:49:42 +0000

    Use `pytest.mark.parametrize`

--- 2021/day6/test_day6.py
+++ 2021/day6/test_day6.py
@@ -12,22 +12,11 @@ def dummy_data() -> np.ndarray:
     return read_data("dummy_day6.csv")
 
 
-def test_num_fish_after_18_days(dummy_data) -> None:
+@pytest.mark.parametrize("days,expected", [(18, 26), (80, 5934)])
+def test_num_fish_after_18_days(dummy_data, days, expected) -> None:
     """
     checks the number of fish as expected
     """
-    days = 18
-    expected = 26
-    computed = compute_number_of_lantern_fish(dummy_data, days)
-    assert computed == expected
-
-
-def test_num_fish_after_80_days(dummy_data) -> None:
-    """
-    checks the number of fish as expected
-    """
-    days = 80
-    expected = 5934
     computed = compute_number_of_lantern_fish(dummy_data, days)
     assert computed == expected
 
