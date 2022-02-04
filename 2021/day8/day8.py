103cc2dc133b7f9f6eb294a68f6e7071156f2bf4
paddyroddy <patrickjamesroddy@gmail.com>
2022-02-04 19:00:32 +0000

    Part 2 getting there

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -2,7 +2,11 @@ import re
 from pathlib import Path
 
 import pandas as pd
-from utils_day8 import DISPLAY, create_columns, find_all_string_lengths
+from utils_day8 import (  # determine_character_mappings,
+    DISPLAY,
+    create_columns,
+    find_all_string_lengths,
+)
 
 _file_location = Path(__file__).resolve()
 _data_path = _file_location.parent
@@ -11,9 +15,10 @@ _data_path = _file_location.parent
 def read_data(filename: str) -> tuple[pd.DataFrame, str]:
     with open(_data_path / filename) as f:
         content = f.read().splitlines()
-    return pd.DataFrame(
+    df = pd.DataFrame(
         [re.split(r"\s\|\s|\s", c) for c in content], columns=create_columns(content)
     )
+    return df.applymap(lambda x: "".join(sorted(x)))
 
 
 def count_digits_1_4_7_8(notes: pd.DataFrame) -> int:
@@ -23,7 +28,7 @@ def count_digits_1_4_7_8(notes: pd.DataFrame) -> int:
     """
     output_df = notes.iloc[:, notes.columns.str.startswith("output")]
     string_lengths = find_all_string_lengths(output_df)
-    numbers_of_interest = [1, 4, 7, 8]
+    numbers_of_interest = {1, 4, 7, 8}
     count = string_lengths.isin(
         DISPLAY.loc[numbers_of_interest].sum(axis="columns").values
     ).values.sum()
@@ -31,20 +36,26 @@ def count_digits_1_4_7_8(notes: pd.DataFrame) -> int:
     return count
 
 
-# def count_total_output_value(notes: pd.DataFrame) -> int:
-#     """
-#     count the total value of each output set of numbers
-#     """
-#     string_lengths = find_all_string_lengths(notes)
-#     for number, details in SEGMENTS.items():
-#         if number != len(LETTERS) + 1:
-#             pass
-#         else:
-#             # if number is 8 then assign to all letters
-#             details["letters"] = LETTERS
+def count_total_output_value(notes: pd.DataFrame) -> int:
+    """
+    count the total value of each output set of numbers
+    """
+    # string_lengths = find_all_string_lengths(notes)
+    # letter_mappings = determine_character_mappings(notes, string_lengths)
+    # one = find_letters_representing_string_length(notes, string_lengths, 1)
+    # seven = find_letters_representing_string_length(notes, string_lengths, 7)
+    # LETTER_MAPPING["a"] = remove_characters_from_df(seven, one)
+    # eight = find_letters_representing_string_length(notes, string_lengths, 8)
+    # zero = find_letters_representing_string_length(notes, string_lengths, 0)
+    # LETTER_MAPPING["d"] = remove_characters_from_df(eight, zero)
+    # six = find_letters_representing_string_length(notes, string_lengths, 6)
+    # five = find_letters_representing_string_length(notes, string_lengths, 5)
+    # LETTER_MAPPING["e"] = remove_characters_from_df(six, five)
+    # nine = find_letters_representing_string_length(notes, string_lengths, 9)
+    return 0
 
 
 if __name__ == "__main__":
     notes = read_data("dummy_day8.txt")
-    count_digits_1_4_7_8(notes)
-    # count_total_output_value(notes)
+    # count_digits_1_4_7_8(notes)
+    count_total_output_value(notes)
--- 2021/day8/utils_day8.py
+++ 2021/day8/utils_day8.py
@@ -1,3 +1,6 @@
+import re
+
+import numpy as np
 import pandas as pd
 
 DISPLAY = pd.DataFrame(
@@ -15,6 +18,34 @@ DISPLAY = pd.DataFrame(
     ],
     columns=["a", "b", "c", "d", "e", "f", "g"],
 )
+
+
+def _find_letters_representing_string_length(
+    df_notes: pd.DataFrame, df_str_len: pd.DataFrame, str_len: int
+) -> pd.DataFrame:
+    """
+    helper method to find the letters that represent the string length
+    https://stackoverflow.com/a/31861396/7359333
+    """
+    return (
+        df_notes.where(df_str_len == DISPLAY.iloc[str_len].sum())
+        .stack()
+        .groupby(level=0)
+        .first()
+        .reindex(df_notes.index)
+    )
+
+
+def _remove_characters_from_df(
+    first_instance: dict[int, pd.DataFrame], more_segments: int, fewer_segments: int
+) -> pd.DataFrame:
+    """
+    removes the characters in df_more_char from df_less_char
+    """
+    # regex used because characters in different orders
+    return first_instance[more_segments].combine(
+        first_instance[fewer_segments], lambda x, y: re.sub(rf"[{y}]", "", x)
+    )
 
 
 def create_columns(content: list[str]) -> list[str]:
@@ -36,3 +67,31 @@ def find_all_string_lengths(df: pd.DataFrame) -> pd.DataFrame:
     helper method to find length of all strings in a dataframe
     """
     return df.applymap(lambda x: len(x))
+
+
+def determine_character_mappings(
+    df_notes: pd.DataFrame, df_str_len: pd.DataFrame
+) -> None:
+    """
+    determine how each character is mapped in the display
+    """
+    # create dataframe to store final mappings
+    letter_mapping = pd.DataFrame(
+        np.empty(DISPLAY.shape, dtype=object), columns=DISPLAY.columns
+    )
+
+    # find first instance of each string length
+    first_instance = dict()
+    for i in range(letter_mapping.shape[0]):
+        first_instance[i] = _find_letters_representing_string_length(
+            df_notes, df_str_len, i
+        )
+
+    # fill in character mapping following rules
+    mapping_dict = dict(a=(7, 1), d=(8, 0), e=(6, 5))
+    for char, (more_segments, fewer_segments) in mapping_dict.items():
+        letter_mapping[char] = _remove_characters_from_df(
+            first_instance, more_segments, fewer_segments
+        )
+
+    return letter_mapping
