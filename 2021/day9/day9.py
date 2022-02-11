960e0b37cf94ad21c18fed9be113cbf9dc239105
paddyroddy <patrickjamesroddy@gmail.com>
2022-02-11 12:59:36 +0000

    Change reading in to `with open`

--- 2021/day9/day9.py
+++ 2021/day9/day9.py
@@ -1,16 +1,26 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 
 _file_location = Path(__file__).resolve()
 _data_path = _file_location.parent
 
 
 def read_data(filename: str) -> np.ndarray:
-    df = pd.read_csv(_data_path / filename, header=None)
-    return df.values[0]
+    with open(_data_path / filename) as f:
+        content = f.read().splitlines()
+    return np.array([list(s) for s in content], dtype=int)
+
+
+def compute_risk_level_sum(heights: np.ndarray) -> int:
+    """
+    compute the sum of the risk levels of the given data
+    """
+    level = 15
+    print(f"Q1 level: {level}")
+    return level
 
 
 if __name__ == "__main__":
-    data = read_data("dummy_day9.csv")
+    heights = read_data("dummy_day9.txt")
+    compute_risk_level_sum(heights)
--- 2021/day9/test_day9.py
+++ 2021/day9/test_day9.py
@@ -1,8 +1,17 @@
 import numpy as np
 import pytest
-from day9 import read_data
+from day9 import compute_risk_level_sum, read_data
 
 
 @pytest.fixture
 def dummy_data() -> np.ndarray:
-    return read_data("dummy_day9.csv")
+    return read_data("dummy_day9.txt")
+
+
+def test_risk_level_as_expected(dummy_data) -> None:
+    """
+    check risk level sum is anticiapted
+    """
+    expected = 15
+    computed = compute_risk_level_sum(dummy_data)
+    assert computed == expected
